e9d8bb97ce88ab31a8e4a061116f018653833762
David T <matkatdavid@gmail.com>
2022-11-17 12:04:18 -0700

    started player class

--- POPO/Player.py
+++ POPO/Player.py
@@ -1,4 +1,23 @@
 class Player:
 
     def __init__(self):
-        pass
+        self.name
+        self.items = []
+
+    def setName(self, name):
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+    def insertItem(self, item):
+        self.items.append(item)
+
+    def isInItems(self, item):
+        if (item.count(item) > 0):
+            return True
+        else:
+            return False
+
+    def getItems(self):
+        return self.items
--- Rooms/Room.py
+++ Rooms/Room.py
@@ -6,3 +6,14 @@ class Room:
         self.items = items
         self.next = next
         self.previous = previous
+
+    def listItems(self):
+        print("Notable items in the room include:")
+        i = 1
+        for item in self.items:
+            print(i + ": " + item)
+            i += 1
+        print("Select the number of the item you would like to interact with")
+
+    def listActions(self, item_index):
+        selection = input()
