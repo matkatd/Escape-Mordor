d6431f7532f55c941e18b83b39af4f7e6681218d
Tim Larsen <timlarsen159@gmail.com>
2022-11-20 22:13:56 -0700

    stufff

--- Rooms/Cell_Start.py
+++ Rooms/Cell_Start.py
@@ -8,7 +8,7 @@ class Cell_Start(Room):
 
     def __init__(self, player):
         super().__init__(f"{player.getName()}'s Cell",
-                         "You find yourself in a jail cell...",
+                         "You find yourself in a jail cell, three walls apear to be solid stone the third is the door. In one corner is a table with a flask, in the other lies a skeleton.",
                          [{
                              "name": "spooky scary skeleton",
                              "actions": ["inspect", "greet"]
@@ -31,12 +31,20 @@ class Cell_Start(Room):
         leave = False
         while leave == False:
             [current_item, current_action, item_index] = self.listItems()
-            if current_item == "torch on the wall" and current_action == "pull torch":
-                print("A secret passage has apeared!")
-                self.items.append({
-                    "name": "secret passage",
-                    "actions": ["exit"]
-                })
+            if current_item == "spooky scary skeleton":
+                if current_action == "inspect":
+                    print("It's a spooky scary skeleton... what did you expect?")
+                if current_action == "greet":
+                    print("The spooky scary skeleton rudely refuses to return your greeting...")
+            if current_item == "torch on the wall":
+                if current_action == "inspect":
+                    print("Just a perfectly ordinary every day torch...")
+                if current_action == "pull torch":
+                    print("A secret passage has apeared!")
+                    self.items.append({
+                        "name": "secret passage",
+                        "actions": ["exit"]
+                    })
             if current_item == "ye olde flask":
                 if current_action == "inspect":
                     print("A old flask lies upon a table, the liquid inside is murky and mysterious...")
@@ -49,7 +57,7 @@ class Cell_Start(Room):
                     self.player.insertItem(current_item)
             if current_item == "cell door":
                 if current_action == "open":
-                    print("your cell door is locked")
+                    print("Your cell door is locked...")
             if current_item == "secret passage" and current_action == "exit":
                 start = Cell_Two(player)
                 start.start_room()
