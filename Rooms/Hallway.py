c62c1f188fdebbda4ae013ba554a371fb43d5481
Tim Larsen <timlarsen159@gmail.com>
2022-11-28 15:41:14 -0700

    stuff

--- Rooms/Hallway.py
+++ Rooms/Hallway.py
@@ -5,6 +5,7 @@ import Rooms.Room as Room
 import Rooms.Cell_Start as Cell_Start
 import Rooms.Cell_Two as Cell_Two
 import Rooms.Main_Chamber as Main_Chamber
+import Puzzles.hallway_cipher as hallway_cipher
 
 class Hallway(Room.Room):
 
@@ -35,15 +36,30 @@ class Hallway(Room.Room):
         while leave == False:
             [current_item, current_action, item_index] = self.listItems()
             if current_item == "hallway exit":
-                if current_action == "":
-                    pass
+                if current_action == "inspect":
+                    print("The exit is a large door, the locking mechanism apppears to require some sort code...")
+                if current_action == "open":
+                    win = hallway_cipher.cipher()
+                    if win == True:
+                        start = Main_Chamber.Main_Chamber()
+                        start.start_room()
+                    else:
+                        print("Better luck next time...")
             if current_item == "gollum's cell":
-                if current_action == "":
-                    pass
+                if current_action == "inspect":
+                    print("Gollum appears to have gone back to sleep...")
+                if current_action == "open":
+                    start = Cell_Two.Cell_Two()
+                    start.start_room()
             if current_item == f"{self.player.getName()}'s cell":
-                if current_action == "":
-                    pass
+                if current_action == "inspect":
+                    print("Your cell appears much smaller from the outside...")
+                if current_action == "open":
+                    start = Cell_Start.Cell_Start()
+                    start.start_room()
             if current_item == "tapestry":
-                if current_action == "":
-                    pass
+                if current_action == "inspect":
+                    print("Maze clue2")
+                if current_action == "lift corner":
+                    print("A blank wall confronts you...")
             
