0cd96eb843c27cc962ddc7b349cc5a171aa14137
chase.rindlisbacher <cjrjazz20@gmail.com>
2022-11-29 16:14:34 -0700

    hangman

--- Puzzles/hangman.py
+++ Puzzles/hangman.py
@@ -1,63 +1,15 @@
 from unicodedata import name
 import random
 
-class Person() :
-    def __init__(self,fname,lname) :
-        self.__first_name = fname
-        self.__last_name = lname
-    def get_fname(self) :
-        return self.__first_name
-    def get_lname(self) :
-        return self.__last_name
-    def set_fname(self) :
-        self.__first_name = input("Enter first name: ")
-    def set_lname(self) :
-        self.__last_name = input("Enter last name: ")
-
-class Games() :
-    def __init__(self,guesscnt) :
-        self.guess_count = guesscnt
-
-# make contestant class inherit from person
-class Contestant(Person) :
-    def __init__(self, fname, lname,gamecount = 0):
-        super().__init__(fname, lname)
-        self.game_count = gamecount
-        self.games_played = []
-    # this method will show how many guesses you took and how many games played
-    def show_results(self) :
-        if self.game_count == 0 :
-            output = self.get_fname() + ' ' + self.get_lname() + ' has not played a game'
-        else :
-            TotalGuesses = 0
-            for g in range(0,len(self.games_played)) : 
-                TotalGuesses += self.games_played[g].guess_count # should add up all the guesses from each game
-            output = f'{self.get_fname()} {self.get_lname()} used a total of {TotalGuesses} guesses'
-        return output
-# made a function to see if the names you enter have any length
-def valid_name(name) :
-    bContinue = True 
-    while bContinue == True :
-        try: 
-            if len(name) > 0 :
-                bContinue = False
-            else : # this will throw an error on purpose to force them to the except to fix the name
-                1 + 'happy'
-        except:
-            name = input("Please enter a valid name:\n")
-            bContinue = True
-    return name
-
 def valid_guess(Guess) :
     bContinue = True 
     while bContinue == True :
         try: 
-            if len(Guess) > 1 or (Guess.isdigit()) :
-                # this will throw an error on purpose to force them to the except to fix the name
-                1 + 'happy'
+            if (len(Guess) > 1) or (Guess.isdigit()) or (len(Guess)) == 0 :
+                raise Exception()
             else:
                 bContinue = False
-        except:
+        except Exception:
             Guess = input("Please enter a single letter guess:\n")
             bContinue = True
     return Guess
@@ -65,13 +17,10 @@ def valid_guess(Guess) :
 lotrNames = ["aragorn", "frodo", "baggins", "elrond", "gandalf","galadriel", "saruman", "sauron"]
 
 # call this function to start a game
-fName = input('Enter player first name: ')
-fName = valid_name(fName) 
-lName = input("Enter players last name: ")
-lName = valid_name(lName)
-def play_game(fname,lname) :
+def play_game() :
+    bWin = False
     guess_count = 0
-
+    print('You have 12 guesses to win this hangman style game. Good luck prisoner.\n')
     iRanNum = random.randint(0,7) # randomly selects a position to use as the index
     # selects the random song
     RandomName = lotrNames[iRanNum]
@@ -85,7 +34,7 @@ def play_game(fname,lname) :
         lstUnderScoredName.append('_') # will use to check against lstNamesLetters
     
     tempLetterList = lstNamesLetters # to use to check for repeated letters
-    oContestant = Contestant(fname,lname)
+    # oContestant = Contestant()
     # initializes the dictionary before the guessing begins
     dictAlphabet = { 'a' : 0, 'b' : 0, 'c' : 0, 'd' : 0, 'e' : 0, 'f' : 0, 'g' : 0, 'h' : 0, 'i' : 0, 'j' : 0, 'k' : 0, 'l' : 0, 'm' : 0, 'n' : 0, 'o' : 0,
     'p' : 0, 'q' : 0, 'r' : 0, 's' : 0, 't' : 0, 'u' : 0, 'v' : 0, 'w' : 0, 'x' : 0, 'y' : 0, 'z' : 0 }
@@ -108,8 +57,10 @@ def play_game(fname,lname) :
                         if lstNamesLetters[cnt] == sGuess :
                             place = lstNamesLetters.index(sGuess)
                             lstUnderScoredName[place] = sGuess # replaces the value stored in this list at the position the letter was last found in the song
-                            tempLetterList[place] = '_'                    
-                    print(lstUnderScoredName)
+                            tempLetterList[place] = '_' 
+                    for l in lstUnderScoredName :
+                        print(l.upper(), end='')    
+                    print('\n')
                 else : # eventually must iterate to this once all instances of a guess have been replaced in the lstUnderScoredName
                     guess_count += 1
                     bContinue2 = False
@@ -120,26 +71,12 @@ def play_game(fname,lname) :
         if (UnderScoredName == RandomName) :
             sOutput = f'Correct! You used {guess_count} guesses\nThe name was {RandomName.capitalize()}'
             print(sOutput)
-            # increment games played and create game object
-            oGame = Games(guess_count)
-            oContestant.game_count += 1
-            # append game object to list of games played
-            oContestant.games_played.append(oGame)
-            print(oContestant.show_results())
+            bContinue1 = False
+            bWin = True
         elif guess_count > 20 :
             print("You took too many guesses!")
-            PlayAgain = input("Would you like to play again (Y:N)\n").upper()
-            bContinue1 = True
-            while bContinue1 :
-                PlayAgain = input("Enter a valid Y or N\n").upper()
-                if PlayAgain == 'Y' :
-                    play_game(fName,lName)
-                    bContinue1 = False
-                elif PlayAgain == 'N' :
-                    print(oContestant.show_results())
-                    bContinue1 = False
-                else :
-                    PlayAgain = input("Enter a valid Y or N")
+            bContinue1 = False
+    return bWin
         
-play_game(fName,lName)
+play_game()
         
